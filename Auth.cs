a6fc4dcc16e7758dba976be6a5a80e05bcee6d75
Admin <Admin@WIN-UKNNBQ77J65>
2024-12-15 23:39:01 +0300

    Добавьте файлы проекта.

---
+++ Auth.cs
@@ -0,0 +1,10 @@
+﻿namespace Dadata_API
+{
+    public class Auth
+    {
+        public const string DadataAuth = "DadataAuth";
+        public string token { get; set; } = string.Empty;
+        public string secret { get; set; } = string.Empty;
+
+    }
+}
---
+++ Controllers/DadataController.cs
@@ -0,0 +1,43 @@
+﻿using Dadata.Model;
+using Dadata;
+using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
+using Dadata_API.Dto;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Dadata_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DadataController : Controller
+    {
+        private readonly IConfiguration Configuration;
+        private readonly IMapper _mapper;
+        public DadataController(IConfiguration configuration, IMapper mapper)
+        {
+            Configuration = configuration;
+            _mapper = mapper;
+        }
+
+        [HttpGet(Name = "GetDadataInfo")]
+        [ProducesResponseType(200, Type = typeof(IAsyncEnumerable<DestinationDto>))]
+        public async IAsyncEnumerable<DestinationDto> GetAsync(string request)
+        {                        
+            var authOptions = new Auth();
+            Configuration.GetSection(Auth.DadataAuth).Bind(authOptions);
+
+            var api = new CleanClientAsync(authOptions.token, authOptions.secret);
+            var address = await api.Clean<Address>(request);
+
+            if (address != null)
+            {
+                var destionation = _mapper.Map<DestinationDto>(address);
+                yield return destionation;
+            }          
+            
+        }
+
+       
+    }
+}
