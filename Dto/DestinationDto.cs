b61c9da6ec23069cb77758f0c2d2b5df03c0620e
Pavel <sulik585@gmail.com>
2024-12-25 23:25:23 +0300

    Добавьте файлы проекта.

---
+++ Dto/DestinationDto.cs
@@ -0,0 +1,19 @@
+﻿using Dadata.Model;
+
+namespace Dadata_API.Dto
+{
+    public class DestinationDto
+    {
+        public string country { get; set; }
+        public string city { get; set; }
+        public string region_with_type { get; set; }
+        public string street { get; set; }
+        public string street_with_type { get; set; }
+        public string house { get; set; }
+        public string flat { get; set; }
+        public string geo_lat { get; set; }
+        public string geo_lon { get; set; }
+        public string fias_id { get; set; }
+
+    }
+}
---
+++ MappingProfiles/DadataProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Dadata.Model;
+using Dadata_API.Dto;
+
+namespace Dadata_API.MappingProfiles
+{
+    public class DadataProfile : Profile
+    {
+        public DadataProfile() {
+            CreateMap<Address, DestinationDto>();
+        }
+    }
+}
